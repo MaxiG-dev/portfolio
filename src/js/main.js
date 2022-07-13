a2b99a699064e204f4f74a81f5964e0c41c77cb8
Maximiliano González <maxig.dev@gmail.com>
2022-07-13 05:02:09 -0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,46 @@
 // ! Variables
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 let stateApp = false;
 
+// ! Events Listeners
 loadPage();
 function loadPage() {
-    document.addEventListener('DOMContentLoaded', eventsListeners)
+    document.addEventListener('DOMContentLoaded', eventsListeners);
 }
-
 function eventsListeners() {
-container.addEventListener('click', () => {
-    startApp('click')
-})
-container.addEventListener('mouseout', () => {
-    startApp('leave')
-})
+    container.addEventListener('click', () => {
+        startApp('click');
+    });
+    container.addEventListener('mouseout', () => {
+        startApp('leave');
+    });
 }
 
-
-
+// ! Functions
 function startApp(e) {
     const audio = document.querySelector('audio');
     if (e === 'click') {
         stateApp = true;
         audio.play();
         audio.volume = 0.5;
-        container.classList.add('containerClick')
-        container.classList.remove('containerHover')
+        container.classList.add('containerClick');
+        container.classList.remove('containerHover');
         setTimeout(() => {
             if (stateApp) {
-                console.log('Eventos cancelado');
+                console.log('Eventos cancelados');
             }
         }, 5000);
     }
     if (e === 'leave') {
         audio.setAttribute('loop', false);
-        container.classList.add('containerHover')
-        container.classList.remove('containerClick')
+        container.classList.add('containerHover');
+        container.classList.remove('containerClick');
         if (stateApp) {
             const timeInterval = setInterval(() => {
-            audio.volume -= 0.1;
+                audio.volume -= 0.1;
             }, 100);
             setTimeout(() => {
-            clearInterval(timeInterval);
+                clearInterval(timeInterval);
             }, 500);
         }
         stateApp = false;
