2e6f1df7ca70511205ce40e38f7b2e2314daf920
Maximiliano González <maxig.dev@gmail.com>
2022-07-13 04:59:42 -0300

    Update

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+// ! Variables
+const container = document.querySelector('.container')
+let stateApp = false;
+
+loadPage();
+function loadPage() {
+    document.addEventListener('DOMContentLoaded', eventsListeners)
+}
+
+function eventsListeners() {
+container.addEventListener('click', () => {
+    startApp('click')
+})
+container.addEventListener('mouseout', () => {
+    startApp('leave')
+})
+}
+
+
+
+function startApp(e) {
+    const audio = document.querySelector('audio');
+    if (e === 'click') {
+        stateApp = true;
+        audio.play();
+        audio.volume = 0.5;
+        container.classList.add('containerClick')
+        container.classList.remove('containerHover')
+        setTimeout(() => {
+            if (stateApp) {
+                console.log('Eventos cancelado');
+            }
+        }, 5000);
+    }
+    if (e === 'leave') {
+        audio.setAttribute('loop', false);
+        container.classList.add('containerHover')
+        container.classList.remove('containerClick')
+        if (stateApp) {
+            const timeInterval = setInterval(() => {
+            audio.volume -= 0.1;
+            }, 100);
+            setTimeout(() => {
+            clearInterval(timeInterval);
+            }, 500);
+        }
+        stateApp = false;
+    }
+}
