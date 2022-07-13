17d9ae72843fe6cd89e0e2e946cdeff157e22e01
Maximiliano González <maxig.dev@gmail.com>
2022-07-13 16:28:12 -0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 // ! Variables
 const container = document.querySelector('.container');
-let stateApp = false;
-
+const audio1 = document.querySelector('#audio1');
+const audio2 = document.querySelector('#audio2');
+const audio3 = document.querySelector('#audio3');
+var timeControl;
+var stateApp;
 // ! Events Listeners
-loadPage();
+loadPage(); // TODO Poner en el body efecto de ***mix-blend-mode: multiply*** para generar efecto en los blancos (como screen en After Effects)
 function loadPage() {
     document.addEventListener('DOMContentLoaded', eventsListeners);
 }
@@ -11,38 +14,44 @@ function eventsListeners() { // TODO comprobar si existe el loader
     container.addEventListener('click', () => {
         startApp('click');
     });
+    // container.addEventListener('dblclick', () => { // TODO Arregla bug doble click
+    //     startApp('click');
+    // });
     container.addEventListener('mouseout', () => {
         startApp('leave');
     });
 }
 
 // ! Functions
+function timeOutControl() {
+    timeControl = setTimeout(() => { // TODO Comprobar si existe el loader
+        container.classList.remove('containerClick');
+        container.classList.add('containerOut')
+        audio2.play();
+        audio2.volume = 0.7;
+        setInterval(() => { // TODO Cmprobar si existe el loader
+        document.querySelector('.loader').remove(); // TODO Cmprobar si existe el loader
+        audio3.play();
+        audio3.volume = 0.2;
+        }, 1000);
+    }, 5000);
+}
+
+function timeOutControlStop() {
+    clearTimeout(timeControl);
+}
 
 function startApp(e) {
-    const audio1 = document.querySelector('#audio1');
-    const audio2 = document.querySelector('#audio2');
-    const audio3 = document.querySelector('#audio3');
     if (e === 'click') {
         stateApp = true;
         audio1.play();
+        audio1.currentTime = 0;
         audio1.volume = 0.5;
         container.classList.add('containerClick');
         container.classList.add('startApp')
         container.classList.remove('containerHover');
         document.querySelector('.loader').classList.add('startAppBg')
-        setTimeout(() => { // TODO Comprobar si existe el loader
-            if (stateApp) {
-                container.classList.remove('containerClick');
-                container.classList.add('containerOut')
-                audio2.play();
-                audio2.volume = 0.7;
-                setInterval(() => { // TODO Cmprobar si existe el loader
-                document.querySelector('.loader').remove(); // TODO Cmprobar si existe el loader
-                audio3.play();
-                audio3.volume = 0.2;
-                }, 1000);
-            }
-        }, 5000);
+        timeOutControl();
     }
     if (e === 'leave') {
         if (document.querySelector('.loader') !== null) {
@@ -52,17 +61,19 @@ function startApp(e) {
             container.classList.remove('containerOut');
             container.classList.remove('startApp');
             document.querySelector('.loader').classList.remove('startAppBg')
-    
             if (stateApp) {
                 const timeInterval = setInterval(() => {
-                    audio1.volume -= 0.1;
+                    if (audio1.volume > 0) {
+                        audio1.volume -= 0.1;
+                    }
                 }, 100);
                 setTimeout(() => {
                     clearInterval(timeInterval);
+                    audio1.pause();
                 }, 500);
             }
+            timeOutControlStop()
             stateApp = false;
         }
-
     }
 }
